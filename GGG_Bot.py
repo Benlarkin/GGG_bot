8315d0f3dc49390c84f62fca7606a71f2863f7c2
ben <benlarkin.usa@gmail.com>
2018-11-29 14:30:29 -0800

    added email functionality to bot

--- GGG_Bot.py
+++ GGG_Bot.py
@@ -4,10 +4,13 @@ import praw
 import pdb
 import re
 import os
+import smtplib
 
 #create a praw instance of Reddit
 reddit = praw.Reddit('g_bot')
-
+#create a server instance via SMTP gmail to send emails
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.login("youremail/username", "yourpassword")
 #keep track of posts using a list
 if not os.path.isfile("prt.txt"):
     prt = []
@@ -19,5 +22,13 @@ else:
         prt = list(filter(None, prt))
 
 subreddit = reddit.subreddit('pathofexile')
+#check the 5 new posts each instance (plan to run every minute on the minute)
 for submission in subreddit.new(limit = 5):
+    #making sure we havne't checked this post yet
     if submission.id not in prt:
+        #obtian te url so we can include it in the email
+        postlink = submission.url
+        #three main content contributors from Grinding Gear Games(handle all PR)
+        if submission.author == "Bex_GGG" or "Mark_GGG" or "chris_wilson":
+            msg = "new post by GGG! link: " + postlink
+            server.sendmail("you@gmail.com", "target@example.com", msg)
